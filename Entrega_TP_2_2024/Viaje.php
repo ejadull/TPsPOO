da2a8c592f0e3034c82ed76775757fb5fd296664
Edgardo Jadull <jadulled@gmail.com>
2024-04-10 01:42:08 -0300

    corrijo menu

--- Entrega_TP_2_2024/Viaje.php
+++ Entrega_TP_2_2024/Viaje.php
@@ -10,12 +10,21 @@ class Viaje
     // Crear instancias de la clase Pasajero y almacenarlas en un array
     private $pasajeros = [];
 
+    /*
     public function __construct($codigo, $destino, $cantidadMaximaPasajeros, $pasajeros)
     {
         $this->pasajeros=$pasajeros;
         $this->codigo=$codigo;
         $this->destino=$destino;
         $this->cantidadMaximaPasajeros=$cantidadMaximaPasajeros;
+    }
+    */
+    public function __construct()
+    {
+        $this->codigo=null;
+        $this->destino=null;
+        $this->cantidadMaximaPasajeros=0;
+        $this->pasajeros=[];
     }
 
     public function __destruct()
@@ -85,7 +94,7 @@ class Viaje
     {
         // Acceder a los objetos en el array y mostrar información
         foreach ($this->pasajeros as $pasajero) {
-            echo $pasajero->__toString() . "<br>";
+            echo $pasajero->__toString() . "\n";
         }
     }
 
@@ -103,6 +112,49 @@ class Viaje
     public function setCodigo($codigo): void
     {
         $this->codigo = $codigo;
+    }
+
+
+    public function tieneViajeCompleto()
+    {
+        return !empty($this->codigo) && !empty($this->destino) && $this->cantidadMaximaPasajeros !== 0;// && count($this->pasajeros)>0;
+    }
+
+    public function viajeEstaCompleto()
+    {
+        return $this->cantidadMaximaPasajeros === count($this->pasajeros);
+    }
+
+    public function existePasajero($dni){
+        $result = false;
+
+        foreach ($this->pasajeros as $pasajero) {
+            $result =  $pasajero->getDni() === $dni;
+            if($result) break;
+        }
+        return $result;
+    }
+
+    public function obtenerPasajero($dni)
+    {
+        $result = false;
+
+        foreach ($this->pasajeros as $pasajero) {
+            if($pasajero->getDni() === $dni){
+                $result = $pasajero;
+            };
+        }
+
+        return $result;
+    }
+
+    public function mostrarInformacion(){
+        echo "INFORMACION DEL VIAJE.\n";
+        echo "CODIGO <". $this->getCodigo() ."> \n";
+        echo "DESTINO: " . $this->getDestino() . "\n";
+        echo "NUM MAX PASAJEROS: " . $this->getCantidadMaximaPasajeros() . "\n\n";
+        echo "PASAJEROS EN VIAJE.\n";
+        $this->listPasajeros();
     }
 }
 
